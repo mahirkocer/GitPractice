792cc120f3dc423c0f6169cdc9c7cee9867a9ddc
Mahir KOÇER <mahirkocer@hotmail.com>
2022-12-09 20:27:01 +0300

    c1

---
+++ src/Store.java
@@ -0,0 +1,17 @@
+public class Store {
+    // features 1
+}
+/*
+Create a new branch: feature-c
+Checkout to feature-c
+	Create a class called Store
+		add some lines
+		commit those changes
+	Update the Store class
+		add more lines
+		commit those changes
+
+	Push the changes to your origin feature-c branch
+
+Create a pull request to merge the feature-c changes to the develop branch
+ */
