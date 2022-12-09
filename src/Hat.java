3ad608c7b32405d16ec4c9b588f19c8c05c7a1e4
Mahir KOÇER <mahirkocer@hotmail.com>
2022-12-09 20:10:51 +0300

    features-b first commit

---
+++ src/Hat.java
@@ -0,0 +1,18 @@
+public class Hat {
+    // Hat class first commit
+}
+/*
+Checkout to feature-b
+	Create a class called Hat
+		ome lines
+		commit tadd shose changes
+	Update the Hat class
+		add more lines
+		commit those changes
+	Update the Hat class
+		add more lines
+		commit those changes
+
+	Push the changes to your origin feature-b branch
+
+ */
